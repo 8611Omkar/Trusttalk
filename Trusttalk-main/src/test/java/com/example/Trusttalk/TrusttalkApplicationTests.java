2173b1ce09082b6b6b518555f12e08f43b5dc66d
Sohan5002 <sohansahu2025@gmail.com>
2025-05-14 14:44:14 +0530

    add MVC Structure

---
+++ Trusttalk-main/src/test/java/com/example/Trusttalk/TrusttalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Trusttalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrusttalkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
