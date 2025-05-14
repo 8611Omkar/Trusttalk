9a6f7dcfffaa71e5d6c346d0337fb7e1fbfa5393
Omkar gupta <99602051+8611Omkar@users.noreply.github.com>
2025-05-14 13:29:11 +0530

    Initial commit: Spring Boot with MongoDB setup

---
+++ src/main/java/com/example/Trusttalk/TrusttalkApplication.java
@@ -0,0 +1,13 @@
+package com.example.Trusttalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrusttalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrusttalkApplication.class, args);
+	}
+
+}
